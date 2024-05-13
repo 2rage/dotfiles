bcc9d964faeaee018ee6fab2c9a1279f468a63a8
2rage <the2rage@icloud.com>
2024-05-13 15:27:14 +0300

    create bootstrap script for windows

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -4,8 +4,31 @@ $sourceDirectory = Split-Path -Parent $MyInvocation.MyCommand.Definition
 # Change directory to the location of bootstrap.ps1
 Set-Location $sourceDirectory
 
-# Pull latest changes from the repository
+# Pull latest changes from the branch "windows"
 git pull origin windows
+
+function Check-PowershellProfile {
+    # Replace single backslashes with double to avoid errors in regular expressions
+    $profileCheck = [regex]::Escape('$powershellProfilePath = "$HOME\.pwsh\powershell_profile.ps1"')
+
+    # Check if $PROFILE already contains the needed string
+    if ((Get-Content $PROFILE | Out-String) -match $profileCheck) {
+        Write-Host "`$powershellProfilePath variable already exists in $PROFILE"
+    } else {
+        # If not, add the entire block of code
+        Add-Content -Path $PROFILE -Value @"
+# Define the path to .pwsh/powershell_profile.ps1
+`$powershellProfilePath = "`$HOME\.pwsh\powershell_profile.ps1`"
+
+# Call .pwsh/powershell_profile.ps1
+. `$powershellProfilePath
+"@
+        Write-Host "Added powershell_profile.ps1 path to $PROFILE"
+    }
+}
+
+Check-PowershellProfile
+
 
 # Function to sync dotfiles to the home directory
 function Sync-Files {
@@ -13,18 +36,19 @@ function Sync-Files {
     $sourceItems = Get-ChildItem -Path $sourceDirectory -Exclude $excludeItems
     foreach ($item in $sourceItems) {
         $destinationPath = Join-Path -Path $HOME -ChildPath $item.Name
+        <# 
+        # Skip copying if the item is the 'Documents' directory
+        if ($item.Name -eq "Documents") {
+            Write-Host "Skipping $($item.FullName)"
+            continue
+        }
+        #>
         if (Test-Path $destinationPath) {
             Write-Host "Overwriting $($item.FullName) -> $($destinationPath)"
         } else {
             Write-Host "Copying $($item.FullName) -> $($destinationPath)"
         }
         Copy-Item -Path $item.FullName -Destination $HOME -Recurse -Force
-    }
-    # Source the appropriate shell configuration file
-    $powershellProfilePath = Join-Path -Path $HOME -ChildPath "Documents\PowerShell\Microsoft.PowerShell_profile.ps1"
-    if (Test-Path $powershellProfilePath) {
-        Write-Host "Sourcing PowerShell profile $($powershellProfilePath)"
-        . $powershellProfilePath
     }
 }
 
